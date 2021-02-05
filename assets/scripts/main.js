d818e52b7e03ba0e535e3bc3f639b7a4058bca4f
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-05 10:46:33 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,19 @@
 // main.js
 
-var hornsound = document.getElementbyId("horn-sound").addEventListener
+// var hornsound = document.getElementbyId("horn-sound").addEventListener
+
+document.getElementbyId("radio-air-horn").addEventListener("click", airHornPic);
+function airHornPic() {
+  document.getElementbyId("sound-image").src = "./assets/media/images/air-horn.svg";
+  document.getElementbyId("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+}
+document.getElementbyId("radio-car-horn").addEventListener("click", carHornPic);
+function carHornPic() {
+  document.getElementbyId("sound-image").src = "./assets/media/images/car.svg";
+  document.getElementbyId("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+}
+document.getElementbyId("radio-party-horn").addEventListener("click", partyHornPic);
+function partyHornPic() {
+  document.getElementbyId("sound-image").src = "./assets/media/images/party-horn.svg";
+  document.getElementbyId("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+}
