eca09db1b8d6dc3fc7e483c2ee7638f68d0b6ff9
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-05 11:31:33 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,19 @@
 // main.js
 
 // horn selection
-document.getElementById("radio-air-horn-container").addEventListener("click", airHornPic);
+document.getElementById("radio-air-horn").addEventListener("click", airHornPic);
 function airHornPic() {
   document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
   document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
 }
-document.getElementById("radio-car-horn-container").addEventListener("click", carHornPic);
+document.getElementById("radio-car-horn").addEventListener("click", carHornPic);
 function carHornPic() {
   document.getElementById("sound-image").src = "./assets/media/images/car.svg";
   document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
 }
-document.getElementById("radio-party-horn-container").addEventListener("click", partyHornPic);
+document.getElementById("radio-party-horn").addEventListener("click", partyHornPic);
 function partyHornPic() {
   document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
   document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
 }
+
