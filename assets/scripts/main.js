1cbf32d2588469921c8dab9daacb5685e5b6059f
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-05 11:04:33 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,18 @@
 // main.js
 
-// var hornsound = document.getElementbyId("horn-sound").addEventListener
-
 // horn selection
-document.getElementbyId("radio-air-horn-container").addEventListener("click", airHornPic);
+document.getElementById("radio-air-horn-container").addEventListener("click", airHornPic);
 function airHornPic() {
-  document.getElementbyId("sound-image").src = "./assets/media/images/air-horn.svg";
-  document.getElementbyId("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+  document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+  document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
 }
-document.getElementbyId("radio-car-horn-container").addEventListener("click", carHornPic);
+document.getElementById("radio-car-horn-container").addEventListener("click", carHornPic);
 function carHornPic() {
-  document.getElementbyId("sound-image").src = "./assets/media/images/car.svg";
-  document.getElementbyId("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+  document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+  document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
 }
-document.getElementbyId("radio-party-horn-container").addEventListener("click", partyHornPic);
+document.getElementById("radio-party-horn-container").addEventListener("click", partyHornPic);
 function partyHornPic() {
-  document.getElementbyId("sound-image").src = "./assets/media/images/party-horn.svg";
-  document.getElementbyId("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+  document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+  document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
 }
